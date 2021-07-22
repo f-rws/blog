63707077ecd6f7f054453f21052894aad124f837
Hiroya Furusawa <pogbba.6@gmail.com>
2021-07-23 00:33:22 +0900

    Add PostContent component

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -2,21 +2,21 @@ import { getAllPostSlugs, getPostBySlug } from '../../lib/posts';
 import { PostData } from '../../types';
 import { Layout } from '../../components/layout/layout';
 import { PageSeo } from '../../components/head/pageSeo';
+import { NextPage } from 'next';
+import { PostContent } from '../../components/post-content/post-content';
 
 type Props = {
   postData: PostData;
 };
 
-const BlogPost: React.VFC<Props> = ({ postData }) => {
-  const { title, date, content } = postData;
+const BlogPost: NextPage<Props> = ({ postData }) => {
+  const { title } = postData;
 
   return (
     <>
       <PageSeo title={title} description={'フルサワのブログです'} />
       <Layout>
-        <h1>{title}</h1>
-        <time>{date}</time>
-        <div>{content}</div>
+        <PostContent postData={postData} />
       </Layout>
     </>
   );
